16cb0f7252312f6e2974af41606a9defbfb0ae19
Lars Djursner Rasmussen <ladr@itu.dk>
2021-01-08 21:25:34 +0100

    awful css only for displaying backend entities

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', routes);
